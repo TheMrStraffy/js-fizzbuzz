a59ee0d8446e90c21f5230e59f54b1858e4f55dc
TheMrStraffy <carmelo.miuccio2@gmail.com>
2022-10-13 16:48:20 +0200

    [added] styles and boxes

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
-//
+const row = document.querySelector('.row');
 
 
 for(i=1; i <= 100; i++){
+    const box = document.createElement('div');
+    box.className = 'box';
   if((i % 3 === 0) && (i % 5 === 0)){
-
+    box.classList.add('fizzBuzz');
+    box.innerHTML = "FIZZBUZZ";
+    row.append(box);
     console.log("Fizz Buzz");
   } else if(i % 3 === 0){
+    box.classList.add('fizz');
+    box.innerHTML = "fizz";
+    row.append(box);
 
     console.log("Fizz");
   } else if (i % 5 === 0){
+    box.classList.add('buzz');
+    box.innerHTML = "buzz";
+    row.append(box);
     console.log("Buzz")
     
   } else{
+    box.innerHTML = i;
+    row.append(box);
     console.log(i);
   }
 }
